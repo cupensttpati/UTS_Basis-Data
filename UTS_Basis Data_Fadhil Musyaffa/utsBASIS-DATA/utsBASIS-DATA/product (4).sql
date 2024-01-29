8db2fb7cbdda540a886d39c982481d7a71ec0beb
cupensttpati <152241079+cupensttpati@users.noreply.github.com>
2024-01-29 23:49:03 +0700

    Add files via upload

---
+++ UTS_Basis Data_Fadhil Musyaffa/utsBASIS-DATA/utsBASIS-DATA/product (4).sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Nov 2023 pada 16.38
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `negara`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(100) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `product_price` bigint(50) NOT NULL,
+  `product_category` varchar(50) NOT NULL,
+  `product_status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_name`, `product_price`, `product_category`, `product_status`) VALUES
+(101, 'Laptop', 7000000, 'ELEKTRONIK', '[value-5]'),
+(102, 'HP', 3000000, 'ELEKTRONIK', '[value-5]'),
+(103, 'TV', 3500000, 'ELEKTRONIK', '[value-5]'),
+(104, 'AC', 8000000, 'ELEKTRONIK', '[value-5]'),
+(105, 'Radio', 200000, 'ELEKTRONIK', 'murah');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
